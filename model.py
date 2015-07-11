e9655a574fdaeee059bfafd57e8296f2f4d273bf
ds2303402 <ds2303402@gmail.com>
2015-07-11 20:35:00 +0000

    deploy

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ def createTrap(id):
     now = time.time()
     cn.insert({"time_stamp_created":now,"lid":id,"clicked":False,"clicked_number":0,"clicks":{}})
 
+'''
 def recordLinkClick(id,ip,ua,ref):
     ts = time.time()
     now = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
@@ -21,6 +22,18 @@ def recordLinkClick(id,ip,ua,ref):
     res["clicked_number"]+=1
     res["clicks"][str(now)]=ip
     cn.save(res)
+'''
+    
+
+def recordLinkClick(id,ip,ua,ref):
+    ts = time.time()
+    now = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    res = cn.find_one({"lid":id})
+    res["clicked"]=True
+    res["clicked_number"]+=1
+    res["clicks"][str(now)]={"ip":ip,"ua":ua,"ref":ref}
+    cn.save(res)
+
 
 def update(id,details):
     res = cn.find_one({"lid":id})
